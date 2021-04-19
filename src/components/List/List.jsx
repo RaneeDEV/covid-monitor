6b07e8d27b74f5eccabed0eb85731c37cf931efe
Ranee <67798875+RaneeDEV@users.noreply.github.com>
2021-04-19 18:11:58 +0300

    Add delta's info

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -15,35 +15,39 @@ export default function List({ covidData }) {
               ))}
             </ul>
             <div className="covidList">
-            <h2 className="listTitle">Confirmed</h2>
+              <h2 className="listTitle">Confirmed</h2>
               {covidData.map((data) => (
-                <p className="confirmed">
-                  {numberFormatter.format(data.confirmed)}
-                </p>
+                <div className="counters confirmed">
+                  <span>{numberFormatter.format(data.confirmed)}</span>
+                  <span>{numberFormatter.format(data.delta_confirmed)}</span>
+                </div>
               ))}
             </div>
             <div className="covidList">
-            <h2 className="listTitle">Deaths</h2>
+              <h2 className="listTitle">Deaths</h2>
               {covidData.map((data) => (
-                <p className="deaths">
-                  {numberFormatter.format(data.deaths)}
-                </p>
+                <div className="counters deaths">
+                  <span>{numberFormatter.format(data.deaths)}</span>
+                  <span>{numberFormatter.format(data.delta_deaths)}</span>
+                </div>
               ))}
             </div>
             <div className="covidList">
-            <h2 className="listTitle">Recovered</h2>
+              <h2 className="listTitle">Recovered</h2>
               {covidData.map((data) => (
-                <p className="recovered">
-                  {numberFormatter.format(data.recovered)}
-                </p>
+                <div className="counters recovered">
+                  <span>{numberFormatter.format(data.recovered)}</span>
+                  <span>{numberFormatter.format(data.delta_recovered)}</span>
+                </div>
               ))}
             </div>
             <div className="covidList">
-            <h2 className="listTitle">Existing</h2>
+              <h2 className="listTitle">Existing</h2>
               {covidData.map((data) => (
-                <p className="existing">
-                  {numberFormatter.format(data.existing)}
-                </p>
+                <div className="counters existing">
+                  <span>{numberFormatter.format(data.existing)}</span>
+                  <span>{numberFormatter.format(data.delta_existing)}</span>
+                </div>
               ))}
             </div>
           </div>

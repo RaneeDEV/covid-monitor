e85044b3e1f613c4742d6ea397c1642db89f0ebd
Ranee <67798875+RaneeDEV@users.noreply.github.com>
2021-04-20 17:57:15 +0300

    World / Ukraine btn data change

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,25 @@ import React, { useState, useEffect } from 'react'
 function App() {
 
     const [covidData, setData] = useState([])
-
+    const [dataLocation, setDataLocation] = useState('world')
     const today = new Date().toJSON().split('T')[0]
-
     useEffect(() => {
       getData(`https://api-covid19.rnbo.gov.ua/data?to=${today}`)
-    }, [])
+    }, [dataLocation])
   
     async function getData(url) {
       const data = await fetch(url).then(r => r.json()) 
-      setData(data.world)
+      setData(data[dataLocation])
+      console.log(data[dataLocation]);
     }
+
+    
     
   return (
-
     <>
     <div className="sideBar">
+      <button className="btn" onClick={() => setDataLocation('ukraine')}>Ukraine</button>
+      <button className="btn" onClick={() => setDataLocation('world')}>World</button>
       <List covidData={covidData} changeCounts={setData}/>
     </div>
     </>
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -28,7 +28,6 @@ export default function List({ covidData, changeCounts }) {
         }
       })
     }
-
     changeCounts(sortedArray)
     setOrder(order * -1)
     console.log(order);
